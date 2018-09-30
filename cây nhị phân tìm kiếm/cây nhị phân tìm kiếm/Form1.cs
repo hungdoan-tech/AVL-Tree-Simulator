3397703df6b1d91dfce1365c852441e2a755f155
Thanh829 <42971407+Thanh829@users.noreply.github.com>
2018-09-30 21:20:09 +0700

    tạo hiệu ứng di chuyển

---
+++ cây nhị phân tìm kiếm/cây nhị phân tìm kiếm/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cây_nhị_phân_tìm_kiếm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int x1, x2, y1, t1, t2, y2;
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (label1.Top < this.Height - 100)
+                label1.Top += +6;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (label1.Left < this.Width - 100)
+                label1.Left += 5;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Size = new Size(67, 53);
+            label1.Location = new Point(x1, y1);
+            label1.Tag = box1.Text;
+            label1.Text = box1.Text;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            x1 = this.Width / 2;
+            x2 = 15;
+            y1 = 15;
+            y2 = this.Height / 2;
+
+        }
+    }
+
+}
---
+++ cây nhị phân tìm kiếm/cây nhị phân tìm kiếm/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("cây nhị phân tìm kiếm")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("cây nhị phân tìm kiếm")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("390e663c-cb69-4961-9278-251812ae902c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
